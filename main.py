1b8e33d2b320f540f37c5a253b2a23ba56e9337c
Wilky <williamwilkerson27@outlook.com>
2022-01-08 17:15:19 -0500

    2nd

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
+from character import Samurai, Wizard, Soldier
+from Enemies import Goblin, Zombie, Goblin_Chief
+import random
+
 """
 In this RPG, You will find yourself in strange times.
-willy is great
-
-
 """
 
+print(f'''
+'~~~~~ Which character do you want to play as ~~~~~' '''
+    )
+print('1: Samurai')
+print('2: Wizard')
+print('3: Soldier')
+user_input = int(input())
 
 
+def main():
+    character = [ Samurai(), Wizard(), Soldier() ]
+    enemies = [ Goblin(), Zombie(), ]
 
-
-
-
-
-def main();
-
+    while character.is_alive():
+        print()
+        print(f'''
+'~~~~~ What do you want to do? ~~~~~' '''
+    )
+    print('1: Explore')
+    print('2: Take Potion')
+    print('3: Rest')
+    user_input = int(input())
