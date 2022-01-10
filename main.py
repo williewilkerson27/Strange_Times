bcd638cb17c63566e577af4acdff6cb87963092a
Wilky <williamwilkerson27@outlook.com>
2022-01-10 17:45:41 -0500

    some thangz

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from character import Samurai, Wizard, Soldier
+from characters import Samurai, Wizard, Soldier
 from Enemies import Goblin, Zombie, Goblin_Chief
 import random
 
@@ -6,22 +6,26 @@ import random
 In this RPG, You will find yourself in strange times.
 """
 
-print(f'''
-'~~~~~ Which character do you want to play as ~~~~~' '''
-    )
-print('1: Samurai')
-print('2: Wizard')
-print('3: Soldier')
-user_input = int(input())
-
-
 def main():
     character = [ Samurai(), Wizard(), Soldier() ]
     enemies = [ Goblin(), Zombie(), Goblin_Chief() ]
 
+    print(f'''
+'~~~~~ Which character do you want to play as ~~~~~' '''
+    )
+    print('1: Samurai')
+    print('2: Wizard')
+    print('3: Soldier')
+    if user_input == 1: 
+        print('You are a Samurai')
+    elif user_input == 2:
+        print('You are a Wizard')
+    else: 
+        print('You are a Soldier')    
+
     while character.is_alive():
-        print()
-        print(f'''
+            print()
+            print(f'''
 '~~~~~ What do you want to do? ~~~~~' '''
     )
     print('1: Explore')
